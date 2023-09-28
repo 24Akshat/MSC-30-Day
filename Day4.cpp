59eddb9ebfb9e86df46e210f8cb5ce61bfd3e37c
Akshat Gupta <133196136+24Akshat@users.noreply.github.com>
2023-09-28 21:24:35 +0530

    Create Day4.cpp

---
+++ Day4.cpp
@@ -0,0 +1,125 @@
+/*
+In this challenge, the task is to debug the existing code to successfully execute all provided test files.
+Input Format
+The input is read by the provided locked code template. In the first line, there is a single integer n denoting the number of rooms booked for today. After that n lines follow. Each of these lines begins with a room_type which is either standard or apartment, and is followed by the number of bedrooms and the number of bathrooms in this room.
+Output Format
+The output is produced by the providedand locked code template. It calculates the total profit by iterating through the vector of all rooms read from the input.
+*/
+
+//Old code:
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HotelRoom {
+public:
+    HotelRoom(int bedrooms, int bathrooms) 
+    : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
+    
+    int get_price() {
+        return 50*bedrooms_ + 100*bathrooms_;
+    }
+private:
+    int bedrooms_;
+    int bathrooms_;
+};
+
+class HotelApartment : public HotelRoom {
+public:
+    HotelApartment(int bedrooms, int bathrooms) 
+    : HotelRoom(bedrooms, bathrooms) {}
+
+    int get_price() {
+        return HotelRoom::get_price() + 100;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<HotelRoom*> rooms;
+    for (int i = 0; i < n; ++i) {
+        string room_type;
+        int bedrooms;
+        int bathrooms;
+        cin >> room_type >> bedrooms >> bathrooms;
+        if (room_type == "standard") {
+            rooms.push_back(new HotelRoom(bedrooms, bathrooms));
+        } else {
+            rooms.push_back(new HotelApartment(bedrooms, bathrooms));
+        }
+    }
+
+    int total_profit = 0;
+    for (auto room : rooms) {
+        total_profit += room->get_price();
+    }
+    cout << total_profit << endl;
+
+    for (auto room : rooms) {
+        delete room;
+    }
+    rooms.clear();
+
+    return 0;
+}
+
+//New Code:
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HotelRoom {
+public:
+    HotelRoom(int bedrooms, int bathrooms) 
+    : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
+    
+    virtual int get_price() {                        //making the function virtual : change 1
+        return 50*bedrooms_ + 100*bathrooms_;
+    }
+protected:                                          // making the variables protected : change 2
+    int bedrooms_;
+    int bathrooms_;
+};
+
+class HotelApartment : public HotelRoom {
+public:
+    HotelApartment(int bedrooms, int bathrooms) 
+    : HotelRoom(bedrooms, bathrooms) {}
+
+    int get_price() {
+        return HotelRoom::get_price() + 100;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<HotelRoom*> rooms;
+    for (int i = 0; i < n; ++i) {
+        string room_type;
+        int bedrooms;
+        int bathrooms;
+        cin >> room_type >> bedrooms >> bathrooms;
+        if (room_type == "standard") {
+            rooms.push_back(new HotelRoom(bedrooms, bathrooms));
+        } else {
+            rooms.push_back(new HotelApartment(bedrooms, bathrooms));
+        }
+    }
+
+    int total_profit = 0;
+    for (auto room : rooms) {
+        total_profit += room->get_price();
+    }
+    cout << total_profit << endl;
+
+    for (auto room : rooms) {
+        delete room;
+    }
+    rooms.clear();
+
+    return 0;
+}
