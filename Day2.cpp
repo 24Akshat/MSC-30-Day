d57204f74d928b1fdfd04f6a8f0966d6f3336aba
Akshat Gupta <133196136+24Akshat@users.noreply.github.com>
2023-09-29 18:09:08 +0530

    Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,4 +1,6 @@
 /*
+Print Pretty
+
 Given a text file with many lines of numbers to format and print, for each row of 3 space-separated doubles, format and print the numbers using the specifications in the Output Format section below.
 Input Format
 The first line contains an integer,T,the number of test cases. 
