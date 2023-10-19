2502f448eb5bdf307fb264395ed9e2dad68a3496
Akshat Gupta <133196136+24Akshat@users.noreply.github.com>
2023-10-19 23:09:14 +0530

    Day25.cpp

--- Day25.cpp
+++ Day25.cpp
@@ -2,3 +2,36 @@
 String to Integer
 Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
 */
+
+//Code Solution:
+class Solution {
+private: 
+    bool check(string &s, int i, int j){
+        while(i<j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }            
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int starting_index = 0;
+        int max_len = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(check(s, i, j)){
+                    if(j-i+1 > max_len){
+                        max_len = j-i+1;
+                        starting_index = i;
+                    }
+                }
+            }
+        }
+        return s.substr(starting_index, max_len);
+    }
+}; 
+  
